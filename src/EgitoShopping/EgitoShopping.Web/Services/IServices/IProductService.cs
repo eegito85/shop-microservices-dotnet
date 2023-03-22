29927f92786eba1c61a5fb2bae56693f23af96e4
Emmanuel Egito Oliveira Candeira Marinho <e_egito@hotmail.com>
2023-03-22 19:52:50 -0300

    Implementar servico de produto no front

---
+++ src/EgitoShopping/EgitoShopping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using EgitoShopping.Web.Models;
+
+namespace EgitoShopping.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAll();
+        Task<ProductModel> FindById(long id);
+        Task<ProductModel> FindByName(ProductModel model);
+        Task<ProductModel> Create(ProductModel model);
+        Task<ProductModel> Update(ProductModel model);
+        Task<bool> DeleteById(long id);
+    }
+}
---
+++ src/EgitoShopping/EgitoShopping.Web/Services/ProductService.cs
@@ -0,0 +1,60 @@
+﻿using EgitoShopping.Web.Models;
+using EgitoShopping.Web.Services.IServices;
+using EgitoShopping.Web.Utils;
+
+namespace EgitoShopping.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _client;
+        public const string BasePath = "api/v1/product";
+
+        public ProductService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<IEnumerable<ProductModel>> FindAll()
+        {
+            var response = await _client.GetAsync(BasePath);
+            return await response.ReadContentAs<IEnumerable<ProductModel>>();
+        }
+
+        public async Task<ProductModel> FindById(long id)
+        {
+            var response = await _client.GetAsync($"{BasePath}/{id}");
+            return await response.ReadContentAs<ProductModel>();
+        }
+
+        public Task<ProductModel> FindByName(ProductModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ProductModel> Create(ProductModel model)
+        {
+            var response = await _client.PostAsJson($"{BasePath}/create", model);
+
+            if(response.IsSuccessStatusCode) { return await response.ReadContentAs<ProductModel>(); }
+
+            throw new Exception("Something went wrong");
+        }
+
+        public async Task<ProductModel> Update(ProductModel model)
+        {
+            var response = await _client.PutAsJson($"{BasePath}/create", model);
+
+            if (response.IsSuccessStatusCode) { return await response.ReadContentAs<ProductModel>(); }
+
+            throw new Exception("Something went wrong");
+        }
+
+        public async Task<bool> DeleteById(long id)
+        {
+            var response = await _client.DeleteAsync($"{BasePath}/{id}");
+            if (response.IsSuccessStatusCode) return await response.ReadContentAs<bool>();
+
+            throw new Exception("Something went wrong");
+        }
+    }
+}
